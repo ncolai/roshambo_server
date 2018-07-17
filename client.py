9364a440da6bd5a4b12f826f16ad565abb31c7b6
Nicholas Lai <nicholaslai749@gmail.com>
2018-07-17 11:35:09 -0700

    Added 2-player option in addition to AI (so two players can connect to the server and play head to head)

--- client.py
+++ client.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
-import socket, sys
+import socket, sys, time
 
 MAX_BYTES = 1024
+MAX_TIME = 0.1
 HOST, PORT = "localhost", 9992
 MOVES = ['rock', 'paper', 'scissors']
 data = " ".join(sys.argv[1:])
 client_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    client_server.connect((HOST, PORT))
-except:
-    print("Couldn't connect to roshambo server...")
-
+start_time = time.clock()
+has_connection = False
+print("Connecting to server...")
 while True:
+    try:
+        client_server.connect((HOST, PORT))
+        has_connection = True
+        break
+    except:
+        has_connection = False
+        if time.clock() - start_time > MAX_TIME:
+            print("Timed out after waiting {} seconds".format(MAX_TIME))
+            break
+
+while has_connection:
     move = raw_input('Ready to play: rock, paper or scissors?')
+    while move not in MOVES: #error checking so we send a correct move to the server
+        move = raw_input('Invalid move choice, try again:')
     client_server.send(move)
+    print("Waiting for opponent...")
     received = client_server.recv(MAX_BYTES)
     if not received:
         continue
@@ -21,7 +34,7 @@ while True:
         print("Not a valid move")
     else:
         status, computer_move = received.split(' ')
-        print("The computer played {}".format(computer_move))
+        print("Your opponent played {}".format(computer_move))
         if status != 'DRAW':
             print("You {}!".format(status))
             break
--- server.py
+++ server.py
@@ -9,30 +9,54 @@ MAX_BYTES       =       1024
 MAX_CONNECTIONS =       5
 MOVES           =       ['rock', 'paper', 'scissors']
 
-#move_intval is the integer value of the PLAYER
-#return win status of player
-def roshambo_turn(conn, move_intval):
-    '''Play one round of roshambo '''
-    result = random.randint(0,2)
-    if move_intval == result:
-        return "DRAW", result
-    elif move_intval == (result + 1) % 3:
-        return "WIN", result
-    else:
-        return "LOSE", result
+#TODO: replace print 'xyz' with print 'xyz\n', (comma included) for thread safety
+def roshambo_ai():
+    '''Simple computer logic to play roshambo'''
+    return random.randint(0,2)
 
-def roshambo_client(conn):
+def roshambo_turn(move1, move2):
+    '''Play one round of roshambo, and return win status of P1'''
+    if move1 == move2:
+        return "DRAW"
+    elif move1 == (move2 + 1) % 3:
+        return "WIN"
+    else:
+        return "LOSE"
+
+def ai_client(conn):
+    '''Client for playing against computer'''
     while True:
         move = conn.recv(MAX_BYTES)
-        if move in MOVES:
-            print(move + ' was sent')
-            status, result = roshambo_turn(conn, MOVES.index(move))
-            print(status + ' ' + MOVES[result])
-            conn.send(status + ' ' + MOVES[result])
-        else:
+        if move not in MOVES:
             conn.send('ERROR')
             break
+        else:
+            print(move + ' was sent')
+            computer_move = roshambo_ai()
+            status = roshambo_turn(MOVES.index(move), computer_move)
+            print(status + ' ' + MOVES[computer_move])
+            conn.send(status + ' ' + MOVES[computer_move])
     conn.close()
+
+def matchup_client(conn0, conn1):
+    '''Client for two players playing against each other'''
+    while True:
+        moves = [conn.recv(MAX_BYTES) for conn in [conn0, conn1]]
+        if moves[0] not in MOVES and moves[1] not in MOVES:
+            break
+        elif moves[0] not in MOVES:
+            moves[0].send('ERROR')
+        elif moves[1] not in MOVES:
+            moves[1].send('ERROR')
+        else:
+            print(moves[0] + ' by P1, ' + moves[1] + ' by P2')
+            status0 = roshambo_turn(MOVES.index(moves[0]), MOVES.index(moves[1]))
+            status1 = roshambo_turn(MOVES.index(moves[1]), MOVES.index(moves[0]))
+            print('P1 ' + status0 + ', P2 ' + status1)
+            conn0.send(status0 + ' ' + moves[1])
+            conn1.send(status1 + ' ' + moves[0])
+    for conn in [conn0, conn1]:
+        conn.close()
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9992
@@ -43,8 +67,9 @@ if __name__ == "__main__":
 
     pool = ThreadPool(processes=MAX_CONNECTIONS)
     while True:
-        conn, addr = server.accept()
-        pool.apply_async(roshambo_client, (conn,))
+        conn0, addr0 = server.accept()
+        conn1, addr1 = server.accept()
+        pool.apply_async(matchup_client, (conn0,conn1,))
 
     server.close()
         
